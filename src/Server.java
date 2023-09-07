3770ceee12bde3b5d02d75f0205c381c6b7f22e9
Ryhor <kave7771@outlook.com>
2023-09-07 16:45:31 +0300

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,21 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server extends SquareEquationSolverImpl {
+    public Server() throws RemoteException {
+    }
+
+    public static void main(String[] args) {
+        try {
+            SquareEquationSolverImpl im = new SquareEquationSolverImpl();
+//            SquareRootSolver solver = (SquareRootSolver) UnicastRemoteObject.exportObject(im, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Solver", im);
+            System.out.println("Server ready");
+        } catch (Exception e) {
+           System.err.println("Server exeption"+e.toString());
+        }
+    }
+}
